b109851703ff819c15eee3153ae95367e0e251c7
wellington <umcorte@gmail.com>
2020-08-30 21:27:55 -0300

    Commit periódico, finalizando os serviços por hoje. Funções de weather estão prontas, faltando expor adequadamente no template

--- weather/views.py
+++ weather/views.py
@@ -34,7 +34,6 @@ def previsao_clima(request):
                                           minima=hoje['minima'],
                                           descricao=hoje['descricao'])
         create.save()
-        create = None
         return hoje
 
     def amanha():
@@ -55,15 +54,90 @@ def previsao_clima(request):
 
         return dois_dias
 
+    def tres_dias():
+        tres_dias = dict()
+        tres_dias['dia'] = date['results']['forecast'][3]['weekday']
+        tres_dias['maxima'] = date['results']['forecast'][3]['max']
+        tres_dias['minima'] = date['results']['forecast'][3]['min']
+        tres_dias['descricao'] = date['results']['forecast'][3]['description']
+
+        return tres_dias
+
+    def quatro_dias():
+        quatro_dias = dict()
+        quatro_dias['dia'] = date['results']['forecast'][4]['weekday']
+        quatro_dias['maxima'] = date['results']['forecast'][4]['max']
+        quatro_dias['minima'] = date['results']['forecast'][4]['min']
+        quatro_dias['descricao'] = date['results']['forecast'][4]['description']
+
+        return quatro_dias
+
+    def cinco_dias():
+        cinco_dias = dict()
+        cinco_dias['dia'] = date['results']['forecast'][5]['weekday']
+        cinco_dias['maxima'] = date['results']['forecast'][5]['max']
+        cinco_dias['minima'] = date['results']['forecast'][5]['min']
+        cinco_dias['descricao'] = date['results']['forecast'][5]['description']
+
+        return cinco_dias
+
+    def seis_dias():
+        seis_dias = dict()
+        seis_dias['dia'] = date['results']['forecast'][6]['weekday']
+        seis_dias['maxima'] = date['results']['forecast'][6]['max']
+        seis_dias['minima'] = date['results']['forecast'][6]['min']
+        seis_dias['descricao'] = date['results']['forecast'][6]['description']
+
+        return seis_dias
+
+    def sete_dias():
+        sete_dias = dict()
+        sete_dias['dia'] = date['results']['forecast'][7]['weekday']
+        sete_dias['maxima'] = date['results']['forecast'][7]['max']
+        sete_dias['minima'] = date['results']['forecast'][7]['min']
+        sete_dias['descricao'] = date['results']['forecast'][7]['description']
+
+        return sete_dias
+
+    def oito_dias():
+        oito_dias = dict()
+        oito_dias['dia'] = date['results']['forecast'][5]['weekday']
+        oito_dias['maxima'] = date['results']['forecast'][5]['max']
+        oito_dias['minima'] = date['results']['forecast'][5]['min']
+        oito_dias['descricao'] = date['results']['forecast'][5]['description']
+
+        return oito_dias
+
+
+
+
     hoje = hoje()
     amanha = amanha()
     dois = dois_dias()
+    tres = tres_dias()
+    quatro = quatro_dias()
+    cinco = cinco_dias()
+    seis = seis_dias()
+    sete = sete_dias()
+    oito = oito_dias()
 
     print(hoje)
     print(amanha)
     print(dois)
+    print(tres)
+    print(quatro)
+    print(cinco)
+    print(seis)
+    print(sete)
+    print(oito)
 
     context = {'hoje': hoje,
                'amanha': amanha,
-               'dois': dois}
+               'dois': dois,
+               'tres': tres,
+               'quatro': quatro,
+               'cinco': cinco,
+               'seis': seis,
+               'sete': sete,
+               'oito': oito}
     return render(request, 'clima.html', context)
